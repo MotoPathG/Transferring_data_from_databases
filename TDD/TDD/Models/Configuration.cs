ba5a0cdda0dc6f755963e3f99747d6df06d7ea48
MotoPath <33699113+MotoPathG@users.noreply.github.com>
2022-02-16 07:51:05 +0300

    The project was initialized and the configuration was created.

---
+++ TDD/TDD/Models/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Models
+{
+    public class Configuration
+    {
+        public DatabaseConfiguration? FromDatabase { get; set; }
+        public DatabaseConfiguration? ToDatabase { get; set; }
+        public TransferTypes TransferType { get; set; }
+        public Configuration(DatabaseConfiguration _fromDatabase, DatabaseConfiguration _toDatabase)
+        {
+            Setup(_fromDatabase, _toDatabase);
+        }
+        public void Setup(DatabaseConfiguration _fromDatabase, DatabaseConfiguration _toDatabase)
+        {
+            FromDatabase = _fromDatabase;
+            ToDatabase = _toDatabase;
+        }
+    }
+    public enum TransferTypes:int { None, SQLServerToPostgreSQL }
+}
---
+++ TDD/TDD/TDD.cs
@@ -0,0 +1,19 @@
+﻿using TDD.Models;
+
+namespace TDD
+{
+    public class TDD
+    {
+        public Configuration TDDConfiguration;
+
+        public TDD(Configuration _configuration)
+        {
+            Setup(_configuration);
+        }
+        
+        public void Setup(Configuration _configuration)
+        {
+            TDDConfiguration = _configuration;
+        }
+    }
+}
